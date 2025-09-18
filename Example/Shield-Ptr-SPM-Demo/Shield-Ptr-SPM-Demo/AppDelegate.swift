991ad5cec474e259b156f5bb457cee10a5e45419
deepakraj.m <deepakraj.m@shield.com>
2025-09-18 15:03:47 +0530

    example app updated wiht new change 1.5.52

--- Example/Shield-Ptr-SPM-Demo/Shield-Ptr-SPM-Demo/AppDelegate.swift
+++ Example/Shield-Ptr-SPM-Demo/Shield-Ptr-SPM-Demo/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import ShieldFraud
+import ShieldPtr
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,44 +14,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let config = Configuration(withSiteId: "SHIELD_SITE_ID", secretKey: "SHIELD_SECRET_KEY")
+        let config = PartnerConfiguration(withSiteId: "SHIELD_SITE_ID", secretKey: "SHIELD_SECRET_KEY")
         config.logLevel = .info
         config.deviceShieldCallback = CallbackExtension()
-        config.enableStopShield = true
+//        config.enableStopShield = true
         config.clientID = "custom_client_id"
         config.userID = "custom_user_id"
-        Shield.setUp(with: config)
+        PartnerShield.setUp(with: config)
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
-class CallbackExtension: DeviceShieldCallback {
-    func didError(error: NSError) {
-        print("FP ERROR: ", error.localizedDescription)
-    }
-    
-    func didSuccess(result: [String: Any]) {
+class CallbackExtension: PartnerDeviceShieldCallback {
+    func didSuccess(siteId: String, result: [String : Any]) {
         print("FP Success: ", result)
 
         NotificationCenter.default.post(name: .ShieldSuccess, object: result, userInfo: result)
         
-        Shield.stopSDK()
+        PartnerShield.stopSDK(for: "SHIELD_SITE_ID")
+    }
+    
+    func didError(siteId: String, error: NSError) {
+        print("FP ERROR: ", error.localizedDescription)
     }
 }
 
